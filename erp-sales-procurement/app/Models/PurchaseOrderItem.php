2180d5747bc73b2d4b71fad3874d9fbe975a4757
Delvintan <delvintan8288@gmail.com>
2026-03-13 18:34:47 +0700

    Delete erp-sales-procurement directory

--- erp-sales-procurement/app/Models/PurchaseOrderItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class PurchaseOrderItem extends Model
-{
-    protected $fillable = ['purchase_order_id', 'item_name', 'qty', 'unit', 'price', 'subtotal'];
-    public function purchaseOrder() { return $this->belongsTo(PurchaseOrder::class); }
-}
--- erp-sales-procurement/app/Models/SalesRequestItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class SalesRequestItem extends Model
-{
-    protected $fillable = ['sales_request_id', 'item_name', 'description', 'qty', 'unit', 'est_price'];
-    public function salesRequest() { return $this->belongsTo(SalesRequest::class); }
-}
